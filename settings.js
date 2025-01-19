a0182b7a76f31790a954c6ca8baf31e8e3fc1f1a
sheddybot <sheddyella23@gmail.com>
2025-01-19 10:14:36 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "OPAY"
 
-global.banknumber = "8133729715"
+global.banknumber = "7058529067"
 
-global.bankowner = "FADARE"
+global.bankowner = "SHADRACK JOSEPH 
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
